5fbb3826103e55c6f0c4aee8cc3df9cddef6fd8a
Stine21 <72187971+Stine21@users.noreply.github.com>
2021-11-29 23:18:32 +0100

    styling a little

--- src/pages/people/index.js
+++ src/pages/people/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-
+import * as styles from '../../styles/people.module.css'
 export default function People() {
     return (
         <Layout>
-        <div>
+        <div className={styles.people}>
             <h2>People</h2>
             <h3>Projects and websites I've Created</h3>
         </div>
